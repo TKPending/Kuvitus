d1f9e8ff71e7cefc41ad97cbb9122ccc8f9e60bf
Tony K. <tony-koke@outlook.com>
2024-06-14 18:37:51 +0100

    Restart Drawing Canvas

--- src/app/drawing/util/updateElements.ts
+++
@@ -1,8 +0,0 @@
-import { DrawingType } from "@/app/types/DrawingType";
-
-export const updateElements = (elements: DrawingType[], newElement: DrawingType, index: number) => {
-    const copyElements: DrawingType[] = [...elements];
-    copyElements[index] = newElement;
-
-    return copyElements;
-};
