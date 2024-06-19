4f1f2d1f71c8477299fd256e4d2fbb2711aceb33
Tony K. <tony-koke@outlook.com>
2024-06-19 18:01:48 +0100

    Added Icons To Zoom Component

--- src/app/components/canvas/CanvasZoomComponent.tsx
+++ src/app/components/canvas/CanvasZoomComponent.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
 type Props = {
   scale: number;
   onZoomIn: () => void;
@@ -6,17 +9,21 @@ type Props = {
 
 const CanvasZoomComponent = ({ scale, onZoomIn, onZoomOut }: Props) => {
   const style =
-    "cursor-pointer hover:scale-105 transition duration-200 h-8 w-8 rounded-lg bg-neutral-300";
+    "flex items-center justify-center cursor-pointer hover:scale-105 transition duration-200 h-8 w-8 rounded-lg bg-neutral-300";
 
   return (
     <div className="flex items justify-center gap-2">
-      <div className={`${style}`} onClick={onZoomOut}></div>
+      <div className={`${style}`} onClick={onZoomOut}>
+        <FontAwesomeIcon icon={faMinus} />
+      </div>
       <div className="w-10 flex items-center justify-center">
         <p className="font-semibold w-12">
           {new Intl.NumberFormat("en-GB", { style: "percent" }).format(scale)}
         </p>
       </div>
-      <div className={`${style}`} onClick={onZoomIn}></div>
+      <div className={`${style}`} onClick={onZoomIn}>
+       <FontAwesomeIcon icon={faPlus} />
+      </div>
     </div>
   );
 };
