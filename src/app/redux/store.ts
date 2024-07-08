7d737c1a6472b298988ed943203b639b851180ac
Tony K. <tony-koke@outlook.com>
2024-07-08 13:59:26 +0100

    Redux update

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,31 +1,20 @@
-import {
-  combineReducers,
-  configureStore,
-  Store,
-  Reducer,
-} from "@reduxjs/toolkit";
-
+import { combineReducers, configureStore, Reducer } from "@reduxjs/toolkit";
 import { GoalType } from "@/app/types/GoalType";
 import { LocalGoalStateType } from "@/app/types/LocalGoalStateType";
 import localGoalsReducer from "./slices/localGoals/localGoalsSlice";
 import goalReducer from "./slices/goal/goalSlice";
-
-interface Action {
-  type: string;
-  payload: any;
-}
 
 interface State {
   goal: GoalType;
   localGoals: LocalGoalStateType;
 }
 
-const rootReducer: Reducer<State, Action> = combineReducers<State>({
+const rootReducer: Reducer<State> = combineReducers({
   goal: goalReducer,
   localGoals: localGoalsReducer,
 });
 
-const store: Store<State, Action> = configureStore({
+const store = configureStore({
   reducer: rootReducer,
 });
 
